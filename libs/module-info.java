4b01246dc5ed871e40b5dd92468ff0cd9106c823
Ian Hoogeboom <github@studio48.nl>
2020-01-10 16:27:40 +0100

    module-info.java

---
+++ libs/module-info.java
@@ -0,0 +1,23 @@
+module net.sf.nmedit.nmlibs {
+//	exports net.sf.nmedit.jpatch;
+//	exports net.sf.nmedit.jpatch.dnd;
+//	exports net.sf.nmedit.jpatch.event;
+//	exports net.sf.nmedit.jpatch.history;
+//	exports net.sf.nmedit.jpatch.history2;
+//	exports net.sf.nmedit.jpatch.impl;
+//	exports net.sf.nmedit.jpatch.js;
+//	exports net.sf.nmedit.jpatch.transform;
+//	exports net.sf.nmedit.jpatch.util;
+	
+//	exports net.sf.nmedit.jsynth;
+//	exports net.sf.nmedit.jsynth.event;
+//	exports net.sf.nmedit.jsynth.midi;
+//	exports net.sf.nmedit.jsynth.worker;
+
+	exports net.sf.nmedit.*;
+	
+	requires commons.logging;
+	requires java.desktop;
+	requires jdk.xml.dom;
+	requires java.xml;
+}
